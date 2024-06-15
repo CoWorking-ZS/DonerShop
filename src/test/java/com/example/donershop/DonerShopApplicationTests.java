e9da7481b7e8b692113f4c1457220b71b51a68f9
Zakhar <vasiliy.vasiliy.03.03@mail.ru>
2024-06-15 15:26:25 +0300

    Initial commit

---
+++ src/test/java/com/example/donershop/DonerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonerShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
